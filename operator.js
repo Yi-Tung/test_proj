74965b00551abc751878b2fa9d67ccb065fd94c8
tung <toDream1225@gmail.com>
2021-09-09 02:27:16 +0800

    刪除測試失敗的程式碼

--- operator.js
+++
@@ -1,9 +0,0 @@
-function sayHi(user) {
-  alert(user);
-}
-
-function sayBye(user) {
-  alert(user);
-}
-
-export {sayHi, sayBye}; // a list of exported variables
--- relay.js
+++
@@ -1,9 +0,0 @@
-import {sayHi, sayBye} from './operator.js';
-
-var tmp = '132';
-
-function initial() {
-	alert(tmp);
-	sayHi('John'); // Hello, John!
-	sayBye('John'); // Bye, John!
-}
